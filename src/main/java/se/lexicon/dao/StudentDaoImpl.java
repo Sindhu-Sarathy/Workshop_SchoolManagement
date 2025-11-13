cc4621edd8af07717dee8a91b8cde297b2975f11
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2025-11-13 15:06:50 +0100

    Added interfaces and implementation for Student

---
+++ src/main/java/se/lexicon/dao/StudentDaoImpl.java
@@ -0,0 +1,79 @@
+package se.lexicon.dao;
+
+import se.lexicon.model.Student;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class StudentDaoImpl implements StudentDao {
+
+    private List<Student> students = new LinkedList<Student>();
+
+    @Override
+    public Student save(Student student) {
+        //Adding the student to the list of students
+        students.add(student);
+
+        //returning the student
+        return student;
+    }
+
+    @Override
+    public Student findByEmail(String email) {
+        for(Student student:students){
+            if(student.getEmail().equalsIgnoreCase(email)){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Student> findByName(String name) {
+        List<Student> studentFound =new LinkedList<>();
+        for(Student student:students){
+            if(student.getName().equalsIgnoreCase(name)){
+                studentFound.add(student);
+            }
+        }
+       if(!studentFound.isEmpty()){
+            return studentFound;
+        }
+        else{
+            return null;
+        }
+    }
+
+    @Override
+    public Student findById(int id) {
+        for(Student student:students){
+            if(student.getId() == id){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return students;
+    }
+
+    @Override
+    public boolean delete(Student student) {
+//To remove by using iterator
+//        Iterator<Student> itr=students.iterator();
+//        while(itr.hasNext()){
+//            Student stud=itr.next();
+//            if(stud.equals(student)){
+//                itr.remove();
+//                return true;
+//            }
+//
+//        }
+//        return false;
+
+        return students.removeIf(o -> o.equals(student));
+    }
+}
