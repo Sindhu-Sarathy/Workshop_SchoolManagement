88e089fa8a9d419f3efd715f16728c5e10571dc8
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2025-11-13 13:16:55 +0100

    Updated the code by adding few methods

--- src/main/java/se/lexicon/model/Course.java
+++ src/main/java/se/lexicon/model/Course.java
@@ -1,5 +1,68 @@
 package se.lexicon.model;
 
-public class Course {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Course {
+    private static int sequencer=0;
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+    private List<Student> students = new LinkedList<Student>();
+
+    public Course(String courseName, LocalDate startDate, int weekDuration) {
+        this.id = sequencer++;
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.weekDuration = weekDuration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+    public void setWeekDuration(int weekDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public void register(Student student)
+    {
+        students.add(student);
+    }
+
+    public void unregister(Student student)
+    {
+        students.remove(student);
+    }
 }
--- src/test/java/se/lexicon/model/StudentTest.java
+++ src/test/java/se/lexicon/model/StudentTest.java
@@ -9,18 +9,17 @@ class StudentTest {
     Student student;
     @BeforeEach
     void SetUp(){
-        student=new Student(1,"Sindhuja","sarathy.sindhu@gmail.com","Östergatan");
+        student=new Student("Sindhuja","sarathy.sindhu@gmail.com","Östergatan");
     }
 
     @Test
     void testConstructors(){
-        int id= student.getId();
         String name= student.getName();
         String email= student.getEmail();
         String address=student.getAddress();
 
 
-        assertEquals(1,id,"The id is incorrect");
+
         assertEquals("Sindhuja",name,"The name is incorrect");
         assertEquals("sarathy.sindhu@gmail.com",email,"The email is incorrect");
         assertEquals("Östergatan",address,"The address is incorrect");
